b9d59433df8d4e9dff6c20baf03dfcfabf361db5
chmery <karektv@gmail.com>
2022-07-17 17:08:56 +0200

    Refactor sass code

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,10 @@
 const allFruitsModal = document.querySelector(".all-fruits");
 const openAllFruitsModal = document.querySelector(".search__show-all");
-const closeAllFruitsModal = document.querySelector(".all-fruits__confirm");
+const closeAllFruitsModal = document.querySelector(".all-fruits__close");
+
+const favouritesModal = document.querySelector(".favourites");
+const openFavouritesModal = document.querySelector(".calculator__btns-favourites");
+const closeFavouritesModal = document.querySelector(".favourites__close");
 
 openAllFruitsModal.addEventListener("click", () => {
     allFruitsModal.showModal();
@@ -8,4 +12,12 @@ openAllFruitsModal.addEventListener("click", () => {
 
 closeAllFruitsModal.addEventListener("click", () => {
     allFruitsModal.close();
+});
+
+openFavouritesModal.addEventListener("click", () => {
+    favouritesModal.showModal();
+});
+
+closeFavouritesModal.addEventListener("click", () => {
+    favouritesModal.close();
 });
