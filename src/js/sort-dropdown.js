55f5d46368280d5926e36585faec08e686f096af
chmery <karektv@gmail.com>
2022-07-19 15:09:57 +0200

    Refactor sort dropdown code

--- src/js/sort-dropdown.js
+++ src/js/sort-dropdown.js
@@ -3,73 +3,76 @@ const sortBtns = document.querySelectorAll("p[class$='sort']");
 const renderDropdownList = (parentEl) => {
     const markup = `
     <div class="dropdown">
-        <p class="dropdown-item">
+        <p class="dropdown-element">
             Calories <i class="fa-solid fa-angle-up"></i>
         </p>
-        <p class="dropdown-item">
+        <p class="dropdown-element">
             Calories <i class="fa-solid fa-angle-down"></i>
         </p>
-        <p class="dropdown-item">
+        <p class="dropdown-element">
             Proteins <i class="fa-solid fa-angle-up"></i>
         </p>
-        <p class="dropdown-item">
+        <p class="dropdown-element">
             Proteins <i class="fa-solid fa-angle-down"></i>
         </p>
-        <p class="dropdown-item">
+        <p class="dropdown-element">
             Fats <i class="fa-solid fa-angle-up"></i>
         </p>
-        <p class="dropdown-item">
+        <p class="dropdown-element">
             Fats <i class="fa-solid fa-angle-down"></i>
         </p>
-        <p class="dropdown-item">
+        <p class="dropdown-element">
             Carbs <i class="fa-solid fa-angle-up"></i>
         </p>
-        <p class="dropdown-item">
+        <p class="dropdown-element">
             Carbs <i class="fa-solid fa-angle-down"></i>
         </p>
     </div> 
     `;
     parentEl.insertAdjacentHTML("beforeend", markup);
-    setListeners(parentEl);
-};
-
-const setListeners = (parentEl) => {
-    const dropdownList = document.querySelector(".dropdown");
-
-    dropdownList.addEventListener("click", (e) => {
-        if (Array.from(dropdownList.childNodes).some((node) => node === e.target)) {
-            removeDropdownList(parentEl);
-        }
-    });
+    setRemovingListener(parentEl);
 };
 
 const removeDropdownList = (parentEl) => parentEl.removeChild(parentEl.querySelector(".dropdown"));
 
+const setRemovingListener = (parentEl) => {
+    const dropdownList = document.querySelector(".dropdown");
+
+    dropdownList.addEventListener("click", (e) => {
+        const isListElementTarget = () => dropdownList.childNodes.some((node) => node === e.target);
+
+        if (isListElementTarget) {
+            removeDropdownList(parentEl);
+        }
+    });
+};
+
 sortBtns.forEach((btn) => {
     btn.addEventListener("click", (e) => {
-        const parentEl = e.target.closest("header");
+        const dropdownList = document.querySelector(".dropdown");
+        const parentEl = btn.closest("header");
         const sortBtn = e.target.closest("p[class$='sort']");
 
-        if (sortBtn === btn && parentEl.contains(parentEl.querySelector(".dropdown"))) {
-            removeDropdownList(parentEl);
-            return;
-        }
-
-        if (sortBtn === btn) {
-            renderDropdownList(parentEl);
-            return;
-        }
+        sortBtn === btn && dropdownList
+            ? removeDropdownList(parentEl)
+            : renderDropdownList(parentEl);
     });
 });
 
 window.addEventListener("click", (e) => {
-    if (
-        Array.from(document.querySelectorAll("[class*='dropdown']")).every(
-            (element) => element !== e.target
-        ) &&
-        document.querySelector(".dropdown") !== null &&
-        Array.from(sortBtns).every((btn) => btn !== e.target.closest("p[class$='sort']"))
-    ) {
-        removeDropdownList(document.querySelector(".dropdown").closest("header"));
+    const dropdownList = document.querySelector(".dropdown");
+    if (!dropdownList) return;
+
+    const parentEl = dropdownList.closest("header");
+    const dropdownElements = Array.from(document.querySelectorAll("[class*='dropdown']"));
+
+    const dropdownElementsNotTarget = () =>
+        dropdownElements.every((element) => element !== e.target);
+
+    const sortBtnsNotTarget = () =>
+        Array.from(sortBtns).every((btn) => btn !== e.target.closest("p[class$='sort']"));
+
+    if (dropdownElementsNotTarget() && sortBtnsNotTarget()) {
+        removeDropdownList(parentEl);
     }
 });
