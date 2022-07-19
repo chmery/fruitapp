7f9a94f7578bbefc8d60513ef0d894f5c3dfa845
chmery <karektv@gmail.com>
2022-07-19 13:12:41 +0200

    Upgrade sort dropdown removing functionality

--- src/js/sort-dropdown.js
+++ src/js/sort-dropdown.js
@@ -30,6 +30,10 @@ const renderDropdownList = (parentEl) => {
     </div> 
     `;
     parentEl.insertAdjacentHTML("beforeend", markup);
+    setListeners(parentEl);
+};
+
+const setListeners = (parentEl) => {
     const dropdownList = document.querySelector(".dropdown");
 
     dropdownList.addEventListener("click", (e) => {
@@ -44,10 +48,28 @@ const removeDropdownList = (parentEl) => parentEl.removeChild(parentEl.querySele
 sortBtns.forEach((btn) => {
     btn.addEventListener("click", (e) => {
         const parentEl = e.target.closest("header");
-        if (e.target === btn && parentEl.contains(parentEl.querySelector(".dropdown"))) {
+        const sortBtn = e.target.closest("p[class$='sort']");
+
+        if (sortBtn === btn && parentEl.contains(parentEl.querySelector(".dropdown"))) {
             removeDropdownList(parentEl);
-        } else {
+            return;
+        }
+
+        if (sortBtn === btn) {
             renderDropdownList(parentEl);
+            return;
         }
     });
+});
+
+window.addEventListener("click", (e) => {
+    if (
+        Array.from(document.querySelectorAll("[class*='dropdown']")).every(
+            (element) => element !== e.target
+        ) &&
+        document.querySelector(".dropdown") !== null &&
+        Array.from(sortBtns).every((btn) => btn !== e.target.closest("p[class$='sort']"))
+    ) {
+        removeDropdownList(document.querySelector(".dropdown").closest("header"));
+    }
 });
