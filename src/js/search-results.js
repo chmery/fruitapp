fa7cef94926f99d98e37767ed300481fd8553b34
chmery <karektv@gmail.com>
2022-07-27 19:09:24 +0200

    Update script and style of search results

--- src/js/search-results.js
+++ src/js/search-results.js
@@ -3,59 +3,78 @@ import { setIconColorOnRender } from "./helpers";
 import { addedToFavourites } from "./add-to-favourites";
 import { addedToCalculator } from "./add-to-calculator";
 
+const searchResults = document.querySelector(".search__results");
+const searchInput = document.querySelector(".search__input");
 let numResults;
 
-const renderResult = (parentEl, markup) => {
-    parentEl.style.opacity = "1";
-    parentEl.insertAdjacentHTML("beforeend", markup);
+const renderResult = (markup) => {
+    searchResults.style.opacity = "1";
+    searchResults.insertAdjacentHTML("beforeend", markup);
     numResults++;
 };
 
-const removeResultsContainer = (parentEl) => {
-    parentEl.innerHTML = "";
-    parentEl.style.opacity = "0";
+const removeResultsContainer = () => {
+    searchResults.innerHTML = "";
+    searchResults.style.opacity = "0";
 };
 
-const setInitialValues = (parentEl) => {
-    parentEl.innerHTML = "";
+const setInitialValues = () => {
+    searchResults.innerHTML = "";
     numResults = 0;
 };
 
-document.querySelector(".search__input").addEventListener("input", (e) => {
+const generateMarkup = (fruit, fruitId) => {
+    const markup = `
+    <div class="search__result item">
+        <div class="search__result-image" style="background-image: url(${fruit.image})"></div>
+        <div class="search__result-text">
+            <p class="search__result-name">${fruit.name}</p>
+            <p class="search__result-kcal bright">${fruit.nutritions.calories} kcal per 100g</p>
+        </div>
+        <div class="search__result-icons">
+            <i class="fa-solid fa-plus fa-lg" data-fruit-id="${fruitId}" ${setIconColorOnRender(
+        addedToCalculator,
+        fruitId
+    )}></i>
+            <i class="fa-regular fa-heart fa-lg" data-fruit-id="${fruitId}" ${setIconColorOnRender(
+        addedToFavourites,
+        fruitId
+    )}></i>
+        </div>
+    </div>
+    `;
+
+    return markup;
+};
+
+searchInput.addEventListener("input", (e) => {
     const inputValue = e.target.value.toLowerCase().trim();
-    const parentEl = document.querySelector(".search__results");
     const MAX_SEARCH_RES = 5;
 
-    setInitialValues(parentEl, numResults);
+    setInitialValues();
 
     fruits.forEach((fruit, i) => {
         if (numResults === MAX_SEARCH_RES) return;
 
-        const markup = `
-        <div class="search__result item">
-            <div class="search__result-image" style="background-image: url(${fruit.image})"></div>
-            <div class="search__result-text">
-                <p class="search__result-name">${fruit.name}</p>
-                <p class="search__result-kcal bright">${fruit.nutritions.calories} kcal per 100g</p>
-            </div>
-            <div class="search__result-icons">
-                <i class="fa-solid fa-plus fa-lg" data-fruit-id="${i}" ${setIconColorOnRender(
-            addedToCalculator,
-            i
-        )}></i>
-                <i class="fa-regular fa-heart fa-lg" data-fruit-id="${i}" ${setIconColorOnRender(
-            addedToFavourites,
-            i
-        )}></i>
-            </div>
-        </div>
-        `;
+        const fruitId = i;
+        const markup = generateMarkup(fruit, fruitId);
 
         const fruitName = fruit.name.toLowerCase();
         const isInputValueInName = fruitName.includes(inputValue);
 
-        if (isInputValueInName) renderResult(parentEl, markup);
+        if (isInputValueInName) renderResult(markup);
     });
 
-    if (!inputValue || (inputValue && !parentEl.innerHTML)) removeResultsContainer(parentEl);
+    if (!inputValue || (inputValue && !searchResults.innerHTML)) removeResultsContainer();
+});
+
+window.addEventListener("click", (e) => {
+    const areSearchResultsTarget =
+        e.target.closest(".search__results") === searchResults ? true : false;
+
+    if (!areSearchResultsTarget) {
+        setInitialValues();
+        removeResultsContainer();
+        searchInput.value = "";
+    }
 });
